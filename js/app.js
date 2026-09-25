39fcc13a113dc176c4bba4b93607b91fcbf11245
sasindudissanayake123 <sasindulokka0@gmail.com>
2026-09-25 10:50:52 +0530

    add customer 1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
+class Customer{
+    name;
+    age;
+    address;
 
-function myFunction(){
-console.log("Hello java");
-
+    Customer(name,age, address){
+this.name = name;
+this.age = age;
+this.address = address;
+    }
 }
+
+let Customer1 = new Customer("SASINDU",30,"GALLE");
+console.log(Customer1);
+let Customer2 = new Customer("dissanayake",60,"homagama");
+console.log(Customer2);
