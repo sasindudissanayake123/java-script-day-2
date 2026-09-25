14df2305bf53968853754023f01457e62636d449
sasindudissanayake123 <sasindulokka0@gmail.com>
2026-09-25 11:29:41 +0530

    add marks

--- js/app.js
+++ js/app.js
@@ -1,16 +1,48 @@
-class Customer{
+/*class Customer{
     name;
     age;
     address;
 
     constructor(name,age, address){
 this.name = name;
-this.age = age;
-this.address = address;
+//this.age = age;
+//this.address = address;
     }
 }
 
-let Customer1 = new Customer("SASINDU",30,"GALLE");
-console.log(Customer1);
-let Customer2 = new Customer("dissanayake",60,"homagama");
-console.log(Customer2);
+//let Customer1 = new Customer("SASINDU",30,"GALLE");
+//console.log(Customer1);
+//let Customer2 = new Customer("Arani",60,"kurunagala");
+//console.log(Customer2);*/
+
+
+let student = [
+{
+    name: "sasindu",
+    age: 20,
+    address: "galle",
+    marks: [
+            {
+                subject: "maths",
+                marks:100
+
+            },
+
+    ]
+},
+{
+    name: "sasi",
+    age: 24,
+    address: "galle"
+},
+{
+    name: "sadu",
+    age: 27,
+    address: "galle"
+},
+
+]
+
+console.log(student);
+console.log(student[1].age);
+console.log(student[0].marks[1]);
